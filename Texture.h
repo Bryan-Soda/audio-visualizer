859ebb9f899e2c487776beec9910daf2c155797f
Bryan Soda <bryansoto5623@gmail.com>
2025-07-21 14:13:22 -0700

    Added needed header files into project

---
+++ Texture.h
@@ -0,0 +1,9 @@
+#ifndef TEXTURE_CLASS_H
+#define TEXTURE_CLASS_H
+
+#include<glad/glad.h>
+#include<stb/stb_image.h>
+
+//#include"shaderClass.h"
+
+#endif // !TEXTURE_CLASS_H
--- main.cpp
+++ main.cpp
@@ -1,12 +1,6 @@
 
 
 int main() {
-<<<<<<< HEAD
 	
 	return 0;
-=======
-	int x = 0;
-
-	return x;
->>>>>>> 714af28ce46735d4b1d9669eb92f333f9aa93e4c
 }
